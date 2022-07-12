d7a607717c22913e0c9ed36e6b99b07ca3221693
Łukasz Stępień <lukasz.stepien.programista@gmail.com>
2022-07-12 23:01:46 +0200

    Created UserMenager class to menage user details

---
+++ UserManager.h
@@ -0,0 +1,47 @@
+#ifndef USERMANAGER_H
+#define USERMANAGER_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+
+#include "User.h"
+//#include "PlikZUzytkownikami.h"
+
+
+using namespace std;
+
+class UserManager
+{
+    int loggedInUserId;
+    vector <User> users;
+
+    User enterNewUserDetails();
+    int getNewUserId();
+    bool whetherLoginExists(string login);
+    string userFileName;  //UsersFile usersFile; /podmieniæ po stworzeniu klasy UseFile
+
+
+public:
+    //UserManager(string nazwaPlikuZuzytkownikami): plikZUzytkownikami(nazwaPlikuZuzytkownikami)
+     UserManager(string newUserFileName): userFileName(newUserFileName)
+    {
+        //uzytkownicy = plikZUzytkownikami.wczytajUzytkownikowZPliku();
+        loggedInUserId = 0;
+    };
+
+    void userRegistration();
+    void setUserId(int newLoggedInUserId);
+    void userLoging();
+    bool ifUserIsLoggedIn();
+    int getLoggedInUserId();
+    void displayAllUsers();
+    void changeLoggedUserPassword();
+    void saveAllUsersToFile();
+    void logoutUser();
+
+};
+
+#endif // USERMANAGER_H
